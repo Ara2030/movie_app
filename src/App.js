2bf4733ce2c2e6fb71b040bee3f392eb688537fa
Araik Margaryan <maraik678@yandex.com>
2023-02-12 09:06:59 +0300

    Final_version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -7,14 +10,45 @@ class App extends React.Component {
     movies: [],
   };
 
+  getMovie = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.jsons?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovie();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading...." : "Страница загружена"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Загрузка...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 export default App;
